7e89fbe9b34bf8c43eec8949d18afad669ced582
Michael Hueter <mthueter@gmail.com>
2017-10-23 15:32:13 -0700

    rock-paper-scissors solution

---
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -0,0 +1,25 @@
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+
+player1 = input("(enter Player 1's choice): ")
+player2 = input("(enter Player 2's choice): ")
+
+print("SHOOT!")
+
+if player1 == "rock" and player2 == "scissors":
+  print("player1 wins")
+elif player1 == "scissors" and player2 == "paper":
+  print("player1 wins")
+elif player1 == "paper" and player2 == "rock":
+  print("player1 wins")
+elif player1 == "rock" and player2 == "paper":
+  print("player2 wins")
+elif player1 == "scissors" and player2 == "rock":
+  print("player2 wins")
+elif player1 == "paper" and player2 == "scissors":
+  print("player2 wins")
+elif player1 == player2:
+  print("it's a draw!")
+else:
+  print("something went wrong.")
