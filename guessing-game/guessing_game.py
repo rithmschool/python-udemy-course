b7fc7eb9f2f43d7c89e952a5844cc5e184bd224e
Michael Hueter <mthueter@gmail.com>
2017-10-25 14:14:28 -0700

    guessing game starter

---
+++ guessing-game/guessing_game.py
@@ -0,0 +1,9 @@
+import random
+
+randomNumber = random.randint(0, 9) + 1  # numbers 1 - 10
+
+# handle user guesses
+#   if they guess correct, tell them they won
+#   otherwise keep guessing
+
+# BONUS - let player play again if they want
