d8b22895b64b25060e445bc1928e388dfbb02e05
Matt Lane <lane.matthew@gmail.com>
2017-10-26 17:11:48 -0700

    updating generators and decorators exercises

--- generators-and-decorators/solutions.py
+++ generators-and-decorators/solutions.py
@@ -1,49 +1,92 @@
 from functools import wraps
+from time import sleep
 
+def week():
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+    for day in days:
+        yield day
 
-def accumulate(end):
-    total = 0
-    for num in range(1, end + 1):
-        total += num
-        yield total
+def make_song(verses=99, beverage="soda"):
+    for num in range(verses, -1, -1):
+        if num > 1:
+            yield f"{num} bottles of {beverage} on the wall."
+        elif num == 1:
+            yield f"Only 1 bottle of {beverage} left!"
+        else:
+            yield f"No more {beverage}!"
 
+def get_multiples(num=1, count=10):
+    next_num = num
+    while count > 0:
+        yield next_num
+        count -= 1
+        next_num += num
 
-def fib(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+def get_unlimited_multiples(num=1):
+    next_num = num
+    while True:
+        yield next_num
+        next_num += num
 
+def next_prime():
+    num = 2
+    all_primes = set()
+    while True:
+        for prime in all_primes:
+            if num % prime == 0:
+                break
+        else:
+            all_primes.add(num)
+            yield num
+        num += num % 2 + 1
 
-def double_result(fn):
+def show_args(fn):
     @wraps(fn)
-    def inner(*args, **kwargs):
-        return fn(*args, **kwargs) * 2
-    return inner
+    def wrapper(*args, **kwargs):
+        print("Here are the args:", args)
+        print("Here are the kwargs:", kwargs)
+        return fn(*args, **kwargs)
+    return wrapper
 
+def ensure_fewer_than_three_args(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if len(args) < 3:
+            return fn(*args, **kwargs)
+        return "Too many arguments!"
+    return wrapper
 
 def only_ints(fn):
     @wraps(fn)
     def inner(*args, **kwargs):
         if any([arg for arg in args if type(arg) != int]):
-            return "Please only invoke with integers"
+            return "Please only invoke with integers."
         return fn(*args, **kwargs)
     return inner
 
-
-def only_even_parameters(fn):
+def ensure_authorized(fn):
     @wraps(fn)
-    def inner(*args, **kwargs):
-        if any([arg for arg in args if arg % 2 != 0]):
-            return "Please add even numbers"
-        return fn(*args, **kwargs)
-    return inner
-
-
-def authorize_user(fn):
-    @wraps(fn)
-    def inner(*args, **kwargs):
-        if kwargs.get('admin') or 'secret' in args:
+    def wrapper(*args, **kwargs):
+        if kwargs.get("role") == "admin":
             return fn(*args, **kwargs)
-        return 'Not Authorized'
+        return "Unauthorized"
+    return wrapper
+
+def delay(timer):
+    def inner(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            print("Waiting {}s before running {}".format(timer, fn.__name__))
+            sleep(timer)
+            return fn(*args, **kwargs)
+        return wrapper
     return inner
+
--- generators-and-decorators/test.py
+++ generators-and-decorators/test.py
@@ -1,72 +1,146 @@
-import solutions
-
+from solutions import *
 import unittest
-
+from unittest.mock import patch, call
 
 class Evaluate(unittest.TestCase):
 
-    def test_accumulate(self):
-        gen = solutions.accumulate(10)
-        self.assertEqual(next(gen), 1)
-        self.assertEqual(next(gen), 3)
-        self.assertEqual(next(gen), 6)
-        self.assertEqual(next(gen), 10)
-        self.assertEqual(next(gen), 15)
-        self.assertEqual(next(gen), 21)
+    def test_week(self):
+        days = week()
+        self.assertEqual(next(days), 'Monday')
+        self.assertEqual(next(days), 'Tuesday')
+        self.assertEqual(next(days), 'Wednesday')
+        self.assertEqual(next(days), 'Thursday')
+        self.assertEqual(next(days), 'Friday')
+        self.assertEqual(next(days), 'Saturday')
+        self.assertEqual(next(days), 'Sunday')
+        with self.assertRaises(StopIteration):
+            next(days)
 
-        sol = [val for val in solutions.accumulate(40)]
-        self.assertEqual(sol, [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210,
-                               231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 496, 528, 561, 595, 630, 666, 703, 741, 780, 820])
+    def test_make_song(self):
+        kombucha_song = make_song(5, "kombucha")
+        self.assertEqual(
+            next(kombucha_song),
+            '5 bottles of kombucha on the wall.'
+        )
+        self.assertEqual(
+            next(kombucha_song),
+            '4 bottles of kombucha on the wall.'
+        )
+        self.assertEqual(
+            next(kombucha_song),
+            '3 bottles of kombucha on the wall.'
+        )
+        self.assertEqual(
+            next(kombucha_song),
+            '2 bottles of kombucha on the wall.'
+        )
+        self.assertEqual(
+            next(kombucha_song),
+            'Only 1 bottle of kombucha left!'
+        )
+        self.assertEqual(
+            next(kombucha_song),
+            'No more kombucha!'
+        )
+        with self.assertRaises(StopIteration):
+            next(kombucha_song)
 
-    def test_fib(self):
-        gen = solutions.fib(10)
-        self.assertEqual(next(gen), 0)
-        self.assertEqual(next(gen), 1)
-        self.assertEqual(next(gen), 1)
-        self.assertEqual(next(gen), 2)
-        self.assertEqual(next(gen), 3)
-        self.assertEqual(next(gen), 5)
-        self.assertEqual(next(gen), 8)
-        self.assertEqual(next(gen), 13)
-        self.assertEqual(next(gen), 21)
-        self.assertEqual(next(gen), 34)
+    def test_make_song_default_arguments(self):
+        default_song = make_song()
+        self.assertEqual(
+            next(default_song),
+            '99 bottles of soda on the wall.'
+        )
 
-        l = [val for val in solutions.fib(15)]
-        sol = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+    def test_get_multiples(self):
+        evens = get_multiples(2, 3)
+        self.assertEqual(next(evens), 2)
+        self.assertEqual(next(evens), 4)
+        self.assertEqual(next(evens), 6)
+        with self.assertRaises(StopIteration):
+            next(evens)
 
-        self.assertEqual(l, sol)
+    def test_get_multiples_default_arguments(self):
+        default_multiples = get_multiples()
+        self.assertEqual(
+            list(default_multiples),
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        )
 
-    def test_double_result(self):
-        @solutions.double_result
-        def add(a, b):
-            return a + b
+    def test_get_unlimited_multiples(self):
+        sevens = get_unlimited_multiples(7)
+        ones = get_unlimited_multiples()
+        self.assertEqual(
+            [next(sevens) for i in range(15)],
+            [7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105]
+        )
+        self.assertEqual(
+            [next(ones) for i in range(20)],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        )
 
-        self.assertEqual(add(2, 2), 8)
+    def test_next_prime(self):
+        primes = next_prime()
+        self.assertEqual(
+            [next(primes) for i in range(25)],
+            [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+                43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        )
+
+    @patch('builtins.print')
+    def test_show_args(self, mock):
+        @show_args
+        def do_nothing(*args, **kwargs):
+            pass
+
+        do_nothing(1, 2, 3,a="hi",b="bye")
+        self.assertTrue(all(c in [
+            call('Here are the args:', (1, 2, 3)),
+            call('Here are the kwargs:', {"a": "hi", "b": "bye"}),
+            call('Here are the args: (1, 2, 3)'),
+            call('Here are the kwargs: {"a": "hi", "b": "bye"}'),
+            call('Here are the args: (1, 2, 3)\nHere are the kwargs: {"a": "hi", "b": "bye"}'),
+            call('Here are the args: (1, 2, 3) \nHere are the kwargs: {"a": "hi", "b": "bye"}')
+        ] for c in mock.call_args_list))
+
+    def test_ensure_fewer_than_three_args(self):
+        @ensure_fewer_than_three_args
+        def add_all(*nums):
+            return sum(nums)
+
+        self.assertEqual(add_all(),0)
+        self.assertEqual(add_all(1),1)
+        self.assertEqual(add_all(1,2),3)
+        self.assertEqual(add_all(1,2,3),"Too many arguments!")
+        self.assertEqual(add_all(1,2,3,4,5,6),"Too many arguments!")
 
     def test_only_ints(self):
-        @solutions.only_ints
-        def add(a, b):
-            return a + b
+        @only_ints 
+        def add(x, y):
+            return x + y
 
-        self.assertEqual(add(2, 2), 4)
-        self.assertEqual(add(2, "2"), "Please only invoke with integers")
+        self.assertEqual(add(1,2),3)
+        self.assertEqual(add("1","2"), "Please only invoke with integers.")
 
-    def test_only_even_parameters(self):
-        @solutions.only_even_parameters
-        def add(a, b):
-            return a + b
+    def test_ensure_authorized(self):
+        @ensure_authorized
+        def show_secrets(*args, **kwargs):
+            return "Shh! Don't tell anybody!"
 
-        self.assertEqual(add(2, 2), 4)
-        self.assertEqual(add(2, 11), "Please add even numbers")
+        self.assertEqual(show_secrets(role="admin"), "Shh! Don't tell anybody!")
+        self.assertEqual(show_secrets(role="nobody"), "Unauthorized")
+        self.assertEqual(show_secrets(a="b"), "Unauthorized")
 
-    def test_authorize_user(self):
-        @solutions.authorize_user
-        def admin_add(*args, **kwargs):
-            return args[0] + args[1]
+    @patch('builtins.print')
+    @patch('solutions.sleep')
+    def test_delay(self, print_mock, sleep_mock):
+        @delay(3)
+        def say_hi():
+            return "hi"
 
-        self.assertEqual(admin_add(2, 2), "Not Authorized")
-        self.assertEqual(admin_add(2, 2, admin=True), 4)
-
+        say_hi()
+        sleep_mock.assert_called_with("Waiting 3s before running say_hi")
+        print_mock.assert_called_with(3)
 
 if __name__ == '__main__':
     unittest.main()
