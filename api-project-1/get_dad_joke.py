7a3335e36a15ef1cf7cdb3cb3d8820123b6ecf53
Matt Lane <lane.matthew@gmail.com>
2017-11-07 16:52:32 -0800

    rename variable

--- api-project-1/get_dad_joke.py
+++ api-project-1/get_dad_joke.py
@@ -2,13 +2,13 @@ import requests
 from random import choice
 
 term = input("Let me tell you a joke! Give me a topic: ")
-response = requests.get(
+response_json = requests.get(
     "https://icanhazdadjoke.com/search",
     headers={"Accept": "application/json"},
     params={"term": term}
 ).json()
-results = response["results"]
-total_jokes = response["total_jokes"]
+results = response_json["results"]
+total_jokes = response_json["total_jokes"]
 if total_jokes > 1:
     print(
         f"I've got {total_jokes} jokes about {term}. Here's one:\n",
