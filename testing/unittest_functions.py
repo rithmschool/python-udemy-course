dd65577549e87327b41f03c003cbd665725461ab
Matt Lane <lane.matthew@gmail.com>
2017-11-16 13:43:57 -0800

    adding testing exercises

---
+++ testing/unittest_functions.py
@@ -0,0 +1,62 @@
+def product():
+    pass
+
+
+def return_day():
+    pass
+
+
+def last_element():
+    pass
+
+
+def number_compare():
+    pass
+
+
+def single_letter_count():
+    pass
+
+
+def multiple_letter_count():
+    pass
+
+
+def list_manipulation():
+    pass
+
+
+def is_palindrome():
+    pass
+
+
+def frequency():
+    pass
+
+
+def flip_case():
+    pass
+
+
+def multiply_even_numbers():
+    pass
+
+
+def capitalize():
+    pass
+
+
+def compact():
+    pass
+
+
+def intersection():
+    pass
+
+
+def partition():
+    pass
+
+
+def mode():
+    pass
