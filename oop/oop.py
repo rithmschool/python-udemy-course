874502ff30c28b99a9f8538de06e3abafaae07f1
mhueter <mthueter@gmail.com>
2017-11-17 15:15:07 -0800

    fixed a bug

--- oop/oop.py
+++ oop/oop.py
@@ -8,7 +8,7 @@ class Card:
         self.value = value
 
     def __repr__(self):
-        return f"{self.value} of {self.suit}"
+        return "{} of {}".format(self.value, self.suit)
 
 
 class Deck:
@@ -19,28 +19,44 @@ class Deck:
         self.cards = [Card(suit, value) for suit in suits for value in values]
 
     def __repr__(self):
-        return f"Deck of {self.count()} cards."
+        return "Deck of {} cards.".format(self.count())
 
     def count(self):
         return len(self.cards)
 
     def _deal(self, num):
+        """
+        Return a list of cards dealt
+        """
         count = self.count()
-        actual = min([num, count])
+        actual = min([num, count])  # make sure we don't try to over-deal
 
         if count == 0:
             raise ValueError("All cards have been dealt")
 
-        return self.cards[-actual:] if num > 1 else self.cards.pop()
+        if actual == 1:
+            return [self.cards.pop()]
+
+        cards = self.cards[-actual:]  # slice off the end
+        self.cards = self.cards[:-actual]  # adjust cards
+
+        return cards
 
     def shuffle(self):
         if self.count() < 52:
             raise ValueError("Only full decks can be shuffled")
+
         shuffle(self.cards)
         return self
 
     def deal_card(self):
-        return self._deal(1)
+        """
+        Returns a single Card
+        """
+        return self._deal(1)[0]
 
     def deal_hand(self, hand_size):
+        """
+        Returns a list of Cards
+        """
         return self._deal(hand_size)
