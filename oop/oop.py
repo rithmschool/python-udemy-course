fbe3e5fe6ad83632090d5fe9af7455b5c0065148
mhueter <mthueter@gmail.com>
2017-11-17 14:44:02 -0800

    oop solution

---
+++ oop/oop.py
@@ -0,0 +1,46 @@
+from random import shuffle
+
+
+class Card:
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+
+class Deck:
+
+    def __init__(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        self.cards = [Card(suit, value) for suit in suits for value in values]
+
+    def __repr__(self):
+        return f"Deck of {self.count()} cards."
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, num):
+        count = self.count()
+        actual = min([num, count])
+
+        if count == 0:
+            raise ValueError("All cards have been dealt")
+
+        return self.cards[-actual:]
+
+    def shuffle(self):
+        if len(self.cards) < 52:
+            raise ValueError("Only full decks can be shuffled")
+        shuffle(self.cards)
+        return self
+
+    def deal_card(self):
+        return self._deal(1)
+
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
